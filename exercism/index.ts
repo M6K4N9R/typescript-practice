0e3e5d0a5201159028fe3c467625906f719ed499
M6K4N9R <zubchevskiy@gmail.com>
2024-09-12 11:42:29 +0200

    Book practice

--- exercism/index.ts
+++ exercism/index.ts
@@ -233,36 +233,66 @@
 
 // ======================================= # 8
 
-export class DnDCharacter {
-  public strength: number;
-  public dexterity: number;
-  public constitution: number;
-  public intelligence: number;
-  public wisdom: number;
-  public charisma: number;
-  public hitpoints: number;
+// export class DnDCharacter {
+//   public strength: number;
+//   public dexterity: number;
+//   public constitution: number;
+//   public intelligence: number;
+//   public wisdom: number;
+//   public charisma: number;
+//   public hitpoints: number;
 
-  constructor() {
-    this.strength = DnDCharacter.generateAbilityScore();
-    this.dexterity = DnDCharacter.generateAbilityScore();
-    this.constitution = DnDCharacter.generateAbilityScore();
-    this.intelligence = DnDCharacter.generateAbilityScore();
-    this.wisdom = DnDCharacter.generateAbilityScore();
-    this.charisma = DnDCharacter.generateAbilityScore();
-    this.hitpoints = 10 + DnDCharacter.getModifierFor(this.constitution);
+//   constructor() {
+//     this.strength = DnDCharacter.generateAbilityScore();
+//     this.dexterity = DnDCharacter.generateAbilityScore();
+//     this.constitution = DnDCharacter.generateAbilityScore();
+//     this.intelligence = DnDCharacter.generateAbilityScore();
+//     this.wisdom = DnDCharacter.generateAbilityScore();
+//     this.charisma = DnDCharacter.generateAbilityScore();
+//     this.hitpoints = 10 + DnDCharacter.getModifierFor(this.constitution);
+//   }
+
+//   public static generateAbilityScore(): number {
+//     const rolls = Array(4).fill(0).map(() => Math.floor(Math.random() * 6) + 1);
+//     return rolls.sort((a, b) => b - a).slice(0, 3).reduce((sum, roll) => sum + roll, 0);
+//   }
+
+//   public static getModifierFor(abilityValue: number): number {
+//     return Math.floor((abilityValue - 10) / 2);
+//   }
+// }
+
+// const mark = new DnDCharacter();
+
+// console.log(mark)
+ 
+
+// ======================== Independant Class practice
+
+export class Book {
+  public title: string;
+  public author: string;
+  public pages: number;
+
+  constructor(title: string, author: string, pages: number) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
   }
 
-  public static generateAbilityScore(): number {
-    const rolls = Array(4).fill(0).map(() => Math.floor(Math.random() * 6) + 1);
-    return rolls.sort((a, b) => b - a).slice(0, 3).reduce((sum, roll) => sum + roll, 0);
+  static getSummary(book: Book): string {
+    return `The book ${book.title} is written by ${book.author}, and has ${book.pages} pages.`
   }
 
-  public static getModifierFor(abilityValue: number): number {
-    return Math.floor((abilityValue - 10) / 2);
+  static comparePages(book1: Book, book2: Book) {
+    return book1.pages > book2.pages ? book1 : book2;
   }
 }
 
-const mark = new DnDCharacter();
+const book1 = new Book("Wealth", "Richard Brandon", 234);
+const book2 = new Book("Mono", "Konyo", 145);
 
-console.log(mark)
- 
+
+console.log(Book.getSummary(book2));
+console.log(Book.comparePages(book1, book2));
+
